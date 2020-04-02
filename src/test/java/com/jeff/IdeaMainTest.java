8fc2f0f0c768f797de806f5f23b20146e1b20a67
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-04-02 10:46:12 +0800

    commit

---
+++ src/test/java/com/jeff/IdeaMainTest.java
@@ -0,0 +1,13 @@
+package com.jeff;
+
+public class IdeaMainTest {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++){
+            System.out.println("------------");
+
+            System.out.println(i);
+        }
+
+    }
+}
