59a2e9a14c4190f215903618ee82230542266e95
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-04-08 10:07:43 +0800

    commit

--- src/test/java/com/jeff/Demo1.java
+++ src/test/java/com/jeff/Demo1.java
@@ -2,9 +2,15 @@ package com.jeff;
 
 public class Demo1 {
 
+    private static  Integer a;
+    Integer b;
     public static void main(String[] args) {
-        int i = 2;
-        i = ++i;
-        System.out.println(i);
+//        int i = 2;
+////        i = ++i;
+////        System.out.println(i);
+
+        System.out.println(a);
+        Demo1 demo1 = new Demo1();
+        System.out.println(demo1.b);
     }
 }
