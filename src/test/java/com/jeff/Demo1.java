e6d9de5f1bab0ab97a0102f643e5f97fa317790b
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-04-16 10:01:53 +0800

    commit

--- src/test/java/com/jeff/Demo1.java
+++ src/test/java/com/jeff/Demo1.java
@@ -9,8 +9,18 @@ public class Demo1 {
 ////        i = ++i;
 ////        System.out.println(i);
 
-        System.out.println(a);
-        Demo1 demo1 = new Demo1();
-        System.out.println(demo1.b);
+//        System.out.println(a);
+//        Demo1 demo1 = new Demo1();
+//        System.out.println(demo1.b);
+        System.out.println(StaticTest.aa);
+        System.out.println(StaticTest2.bb);
     }
+}
+//  -XX:+TraceClassLoading
+class StaticTest{
+    public final  static String aa = "aaaaaaaaaaaa";
+}
+
+class StaticTest2{
+    public static String bb = "ccccccccccccccc";
 }
