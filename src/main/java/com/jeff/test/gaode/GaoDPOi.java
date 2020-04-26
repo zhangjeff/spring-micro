ee58a228a8d258731765f88a53db4afa0030c006
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-04-26 17:02:21 +0800

    commit

---
+++ src/main/java/com/jeff/test/gaode/GaoDPOi.java
@@ -0,0 +1,34 @@
+package com.jeff.test.gaode;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class GaoDPOi {
+
+    public static void main(String[] args) throws Exception {
+        String location = "116.473168,39.993015";
+        String keywords = "美食";
+        generateData(location, keywords);
+    }
+
+    public static void  generateData(String location, String keywords) throws Exception{
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String radius = "500";
+        String url = "https://restapi.amap.com/v3/place/around?key=eea76fff9ed991bb945380e7ba40dfe5&location="+location+"&keywords="+
+                keywords+"&radius=" + radius;
+        HttpGet get = new HttpGet(url);
+        CloseableHttpResponse response = httpClient.execute(get);
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println(statusCode);
+        HttpEntity entity = response.getEntity();
+        String string = EntityUtils.toString(entity, "utf-8");
+        System.out.println(string);
+        //关闭httpclient
+        response.close();
+        httpClient.close();
+    }
+}
